640706c0d3c2fed18e762b4ebdca51e403268e36
bob5ec <bob5ec@users.noreply.github.com>
2019-01-06 14:49:15 +0100

    initial samba setup without user and password

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,5 +3,6 @@
 compose=$1
 
 #for compose in *.yml; do
-  docker deploy --compose-file $compose `basename $compose .yml`
+docker-compose -f $compose -p `basename $compose .yml` down
+docker-compose -f $compose -p `basename $compose .yml` up
 #done
