bca95ca28eb5bd2789264e65efae6b57d6797f3f
bob5ec <bob5ec@users.noreply.github.com>
2019-02-03 14:22:37 +0100

    fix file permissions

--- samba/bin/start.sh
+++ samba/bin/start.sh
@@ -2,6 +2,9 @@
 for u in `ls /secrets`; do
   echo adding $u
   adduser -s /sbin/nologin -h /home/$u -H -D $u
+  mkdir -p /home/$u
+  chown $u /home/$u
+  chmod 700 /home/$u
   # thank you smbpasswd for having such a crapy api
   (cat /secrets/$u; cat /secrets/$u) | smbpasswd -s -a $u
 done
