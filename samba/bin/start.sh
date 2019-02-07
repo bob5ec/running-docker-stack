8476fc6283c01ce37b441370271cd8ff5c15ce47
bob5ec <bob5ec@users.noreply.github.com>
2019-02-07 22:59:37 +0100

    use jq to read complex user objects

--- samba/bin/start.sh
+++ samba/bin/start.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
-for u in `ls /secrets`; do
+addgroup --gid 100 users
+
+COUNT=`cat /secrets/users | jq '.users|length'`
+for ((i=1;i<COUNT;i++)); do
+  u=`cat /secrets/users | jq ".users[$i].name" -r`
+  id=`cat /secrets/users | jq ".users[$i].id" -r`
   echo adding $u
-  adduser -s /sbin/nologin -h /home/$u -H -D $u
+  adduser -s /sbin/nologin -h /home/$u --uid $id --group users -H -D $u
   mkdir -p /home/$u
   chown $u /home/$u
   chmod 700 /home/$u
-  # thank you smbpasswd for having such a crapy api
-  (cat /secrets/$u; cat /secrets/$u) | smbpasswd -s -a $u
+  # thank you smbpasswd for having such a crappy api
+  (cat /secrets/users | jq ".users[$i].password" -r; cat /secrets/users | jq ".users[$i].password" -r) | smbpasswd -s -a $u
 done
 
 smbd --foreground --log-stdout
