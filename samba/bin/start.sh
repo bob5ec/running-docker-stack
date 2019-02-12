c8f1b52297da3111f9c42eab287f1dfceabaf865
bob5ec <bob5ec@users.noreply.github.com>
2019-02-12 17:51:09 +0100

    fix sh -> bash script

--- samba/bin/start.sh
+++ samba/bin/start.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-#addgroup -g 100 users
 
 COUNT=`cat /secrets/users | jq '.users|length'`
-echo adding $COUNT users
 for ((i=0;i<COUNT;i++)); do
   u=`cat /secrets/users | jq ".users[$i].name" -r`
   id=`cat /secrets/users | jq ".users[$i].id" -r`
-  echo adding $u
-  adduser -s /sbin/nologin -h /home/$u --u $id --group users -H -D $u
+  adduser -s /sbin/nologin -h /home/$u -u $id -g users -H -D $u
   mkdir -p /home/$u
   chown $u /home/$u
   chmod 700 /home/$u
