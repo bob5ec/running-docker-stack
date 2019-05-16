4a1c1d801688c4f5ff712bc5a088d80ee0245bbc
bob5ec <bob5ec@users.noreply.github.com>
2019-05-16 22:33:04 +0200

    process ids are not deterministic??

--- backup/test/test.sh
+++ backup/test/test.sh
@@ -6,7 +6,9 @@ sleep 5
 echo TEST: container comes up
 docker ps|grep backuptest_backup_1 || exit 1
 
-echo TEST: cron is running as process number 7... there is no ps available
-docker exec -it backuptest_backup_1 cat /proc/7/status |grep cron || exit 1
+echo TEST: cron is running... there is no ps available
+docker exec -it backuptest_backup_1 cat /proc/*/status |grep cron || exit 1
+
+#docker exec -it backuptest_backup_1 /bin/bash
 
 docker-compose -f docker-compose.yml -p backup-test down
