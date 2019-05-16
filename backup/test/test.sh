d8aff9ca6ee6952cb8f891ee6b9047449d61abbf
bob5ec <bob5ec@users.noreply.github.com>
2019-05-16 22:26:45 +0200

    wait for container to come up

--- backup/test/test.sh
+++ backup/test/test.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 docker-compose -f docker-compose.yml -p backup-test up -d
 
-echo container comes up
+sleep 5
+
+echo TEST: container comes up
 docker ps|grep backuptest_backup_1 || exit 1
 
-echo cron is running as process number 7... there is no ps available
+echo TEST: cron is running as process number 7... there is no ps available
 docker exec -it backuptest_backup_1 cat /proc/7/status |grep cron || exit 1
 
 docker-compose -f docker-compose.yml -p backup-test down
