745218589b6b5d2c99612d984f8a42115821a322
bob5ec <bob5ec@users.noreply.github.com>
2019-05-20 21:26:46 +0200

    * create test data for backup scripts * add sshd container * create ssh keys for accessing sshd test container

--- backup/test/test.sh
+++ backup/test/test.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 docker-compose -f docker-compose.yml -p backup-test up -d
 
-sleep 5
+#TODO connect to the test-sshd
+#docker exec -it backuptest_backup_1 ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no test@test-sshd
+
+#docker exec -it backuptest_test-sshd_1 /bin/bash
+#sleep 5
 
 echo TEST: container comes up
 docker ps|grep backuptest_backup_1 || exit 1
@@ -9,6 +13,5 @@ docker ps|grep backuptest_backup_1 || exit 1
 echo TEST: cron is running... there is no ps available
 docker exec -it backuptest_backup_1 cat /proc/*/status |grep cron || exit 1
 
-#docker exec -it backuptest_backup_1 /bin/bash
 
 docker-compose -f docker-compose.yml -p backup-test down
