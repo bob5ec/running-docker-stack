6271b85e399b2f353dce5aa30b881297efb3e58f
bob5ec <bob5ec@users.noreply.github.com>
2019-05-20 21:44:35 +0200

    * test the ssh connection

--- backup/test/test.sh
+++ backup/test/test.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 docker-compose -f docker-compose.yml -p backup-test up -d
 
-#TODO connect to the test-sshd
-#docker exec -it backuptest_backup_1 ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no test@test-sshd
+echo waiting for docker containers to start ...
+sleep 3
 
+echo TEST: connect to the test-sshd
+docker exec -it backuptest_backup_1 ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no test@test-sshd -C "echo ... connection OK" || exit 1
+
+#docker exec -it backuptest_backup_1 /bin/bash
 #docker exec -it backuptest_test-sshd_1 /bin/bash
-#sleep 5
 
 echo TEST: container comes up
 docker ps|grep backuptest_backup_1 || exit 1
