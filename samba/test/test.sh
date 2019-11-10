2b80aabf4f00c038acd4c1536d49edd57a206ca9
bob5ec <bob5ec@users.noreply.github.com>
2019-11-10 14:15:04 +0100

    add ideas for tests

--- samba/test/test.sh
+++ samba/test/test.sh
@@ -27,34 +27,11 @@ SHARES=`docker exec -it samba-client /bin/sh -c "echo test | smbclient -U foo -L
 echo $SHARES
 [[ $SHARES =~ "data" && $SHARES =~ "foo" ]] || cleanup 1
 
-# data share is public :-(
+# TODO data share is public :-(
 #echo TEST: do NOT expose data share without authentication
 #docker exec -it samba-client /bin/sh -c "echo '' | smbclient -L samba | grep data" && cleanup 1
 
-#echo TEST: connect to the test-sshd
-#docker exec backuptest_backup_1 ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no test@test-sshd -C "echo ... connection OK" || exit 1
-
-# TEST case with existing last link
-#docker exec backuptest_test-sshd_1 /bin/bash -c "ln -s /tmp /home/test/last" || exit 1
-
-# create target folder
-#BASE_DIR=/home/test/backup
-#docker exec backuptest_test-sshd_1 /bin/bash -c "mkdir -p $BASE_DIR; chown test $BASE_DIR"
-
-#echo TEST: run backup
-#docker exec backuptest_backup_1 /etc/cron.daily/backup.sh || exit 1
-
-#echo TEST: backup exists on storage
-#BASE_DIR=/home/test/backup
-#docker exec backuptest_test-sshd_1 /bin/bash -c "[ -f $BASE_DIR/last/home/1 ] && [ ! -f $BASE_DIR/last/home/exclude1 ] && [ ! -f $BASE_DIR/last/home/exclude.a ] && [ -f $BASE_DIR/last/folder/2 ] && [ ! -f $BASE_DIR/last/folder/1.exclude ]" || exit 1
-
-# do we really need an extra mirror??? -> remove it if not needed
-#echo TEST: run mirror
-#docker exec backuptest_backup_1 /etc/cron.daily/mirror.sh || exit 1
-#
-#echo TEST: mirror exists on storage
-#BASE_DIR=/home/test/backup
-#docker exec backuptest_test-sshd_1 /bin/bash -c "[ -f $BASE_DIR/last/home/1 ] && [ ! -f $BASE_DIR/last/home/exclude1 ] && [ ! -f $BASE_DIR/last/home/exclude.a ] && [ -f $BASE_DIR/last/folder/2 ] && [ ! -f $BASE_DIR/last/folder/1.exclude ]" || exit 1
+# TODO echo TEST: read and write files
 
 #DEBUG
 #docker exec -it backuptest_backup_1 /bin/bash
