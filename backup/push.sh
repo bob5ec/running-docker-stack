556cfdd5b065f3b504a72137901b5fd00d99bce7
bob5ec <bob5ec@users.noreply.github.com>
2020-03-19 21:54:38 +0100

    changing backup image tag

--- backup/push.sh
+++ backup/push.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 source ../build-system.sh
 
+docker image tag bob5ec/backup:new bob5ec/backup:$ENV
 # push the image
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push bob5ec/backup:$ENV
