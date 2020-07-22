11832761911c8a4db0037f8f923a0aa88655e0ba
bob5ec <bob5ec@users.noreply.github.com>
2020-07-22 22:57:28 +0200

    remove local testing

--- 5nextcloud/test/test.sh
+++ 5nextcloud/test/test.sh
@@ -30,7 +30,7 @@ DOCKER_DEPLOY="/tmp/docker-deploy"
 curl -o $DOCKER_DEPLOY https://raw.githubusercontent.com/bob5ec/docker-infrastructure/prod/roles/docker/files/docker-deploy
 chmod +x $DOCKER_DEPLOY
 # test local docker-deploy
-DOCKER_DEPLOY="../../../docker-infrastructure/roles/docker/files/docker-deploy"
+#DOCKER_DEPLOY="../../../docker-infrastructure/roles/docker/files/docker-deploy"
 
 $DOCKER_DEPLOY -l ../..
 
